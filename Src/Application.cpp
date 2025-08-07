fb307038da9e0d43d75e241dcdc042503c53f212
ASOST\2316004 <dekirusunsun410@gmail.com>
2025-08-07 10:32:56 +0900

    main

---
+++ Src/Application.cpp
@@ -0,0 +1,107 @@
+#include <DxLib.h>
+#include <EffekseerForDXLib.h>
+#include "Application.h"
+
+Application* Application::instance_ = nullptr;
+
+const std::string Application::PATH_IMAGE = "Data/Image/";
+const std::string Application::PATH_MODEL = "Data/Model/";
+const std::string Application::PATH_EFFECT = "Data/Effect/";
+
+void Application::CreateInstance(void)
+{
+	if (instance_ == nullptr)
+	{
+		instance_ = new Application();
+	}
+	instance_->Init();
+}
+
+Application& Application::GetInstance(void)
+{
+	return *instance_;
+}
+
+void Application::Init(void)
+{
+
+	// アプリケーションの初期設定
+	SetWindowText("ContestBaseProject");
+
+	// ウィンドウサイズ
+	SetGraphMode(SCREEN_SIZE_X, SCREEN_SIZE_Y, 32);
+	ChangeWindowMode(true);
+
+	// DxLibの初期化
+	SetUseDirect3DVersion(DX_DIRECT3D_11);
+	isInitFail_ = false;
+	if (DxLib_Init() == -1)
+	{
+		isInitFail_ = true;
+		return;
+	}
+
+	// Effekseerの初期化
+	InitEffekseer();
+
+	// キー制御初期化
+	SetUseDirectInputFlag(true);
+
+}
+
+void Application::Run(void)
+{
+
+	// ゲームループ
+	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
+	{
+
+		ScreenFlip();
+
+	}
+
+}
+
+void Application::Destroy(void)
+{
+
+	// Effekseerを終了する。
+	Effkseer_End();
+
+	// DxLib終了
+	if (DxLib_End() == -1)
+	{
+		isReleaseFail_ = true;
+	}
+
+	delete instance_;
+
+}
+
+bool Application::IsInitFail(void) const
+{
+	return isInitFail_;
+}
+
+bool Application::IsReleaseFail(void) const
+{
+	return isReleaseFail_;
+}
+
+Application::Application(void)
+{
+	isInitFail_ = false;
+	isReleaseFail_ = false;
+}
+
+void Application::InitEffekseer(void)
+{
+	if (Effekseer_Init(8000) == -1)
+	{
+		DxLib_End();
+	}
+
+	SetChangeScreenModeGraphicsSystemResetFlag(FALSE);
+
+	Effekseer_SetGraphicsDeviceLostCallbackFunctions();
+}
