a09d54467c4bbd0a3f8fb10047b447566b92d4e3
ASOST\2316004 <dekirusunsun410@gmail.com>
2025-08-07 14:22:29 +0900

    ベースクラスの作成

---
+++ Src/Object/Target/TargetBase.cpp
@@ -0,0 +1,25 @@
+#include "TargetBase.h"
+
+TargetBase::TargetBase(void)
+{
+}
+
+TargetBase::~TargetBase(void)
+{
+}
+
+void TargetBase::Init(void)
+{
+}
+
+void TargetBase::Update(void)
+{
+}
+
+void TargetBase::Draw(void)
+{
+}
+
+void TargetBase::Release(void)
+{
+}
---
+++ Src/Object/Target/TargetBase.h
@@ -0,0 +1,27 @@
+#pragma once
+#include<functional>
+#include"../ObjectBase.h"
+class TargetBase :public ObjectBase
+{
+
+public:
+
+	// コンストラクタ
+	TargetBase(void);
+
+	// デストラクタ
+	virtual ~TargetBase(void);
+
+	virtual void Init(void);
+	virtual void Update(void);
+	virtual void Draw(void);
+	virtual void Release(void);
+
+protected:
+	enum class STATE
+	{
+		NONE
+	};
+private:
+
+};
